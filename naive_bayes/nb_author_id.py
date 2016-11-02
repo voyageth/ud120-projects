e9ade4c8168e7afe301671d5170aefbd8c7551ee
voyageth <voyageth@naver.com>
2016-11-02 15:04:38 +0900

    feat: Impl Naive Bayes mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred_labels_test = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred_labels_test)
+print("accuracy : {}".format(accuracy))
+
+from sklearn.metrics import classification_report
+print(classification_report(labels_test, pred_labels_test, target_names=['Sara', 'Chris']))
 
 
 #########################################################
-
-
