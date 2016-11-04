7906c4a49a418b55170362714a22df19f7c72f35
voyageth <voyageth@naver.com>
2016-11-04 11:47:25 +0900

    feat: Impl SVM mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,5 +26,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+print('start')
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C=10000.0)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
+clf.fit(features_train, labels_train)
+print('fit end')
+
+pred = clf.predict(features_test)
+print('predict end')
+
+cnt = 0
+for i in range(0, len(pred)) :
+    if pred[i] == 1 :
+        cnt+=1
+
+print ('count', cnt)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print("accuracy : {}".format(accuracy))
 
