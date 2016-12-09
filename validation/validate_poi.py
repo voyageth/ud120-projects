b838494979a9a9dd618e3d40eb8fb80f389907cc
voyageth <voyageth@naver.com>
2016-12-09 16:20:44 +0900

    feat: Impl validation part

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,18 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn import cross_validation
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+pred_labels_test = clf.predict(X_test)
 
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, pred_labels_test)
+print("accuracy : {}".format(accuracy))
